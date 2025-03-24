0cdc9ef0a7437d2e6c3dc8528d4c08c3895fe25b
tangoctrung <58180752+tangoctrung@users.noreply.github.com>
2025-03-24 20:45:45 +0700

    Update configUrl.js

--- src/config/configUrl.js
+++ src/config/configUrl.js
@@ -1,3 +1,3 @@
-export const baseUrl = process.env.REACT_APP_ENV === "local" ? "http://localhost:8800/api" : "https://socialtnt-server.onrender.com/api";
-export const baseUrlSocket = process.env.REACT_APP_ENV === "local" ? "http://localhost:8800" : "https://socialtnt-server.onrender.com";
+export const baseUrl = process.env.REACT_APP_ENV === "local" ? "http://localhost:8800/api" : "https://socialtnt.leevyy.click/api";
+export const baseUrlSocket = process.env.REACT_APP_ENV === "local" ? "http://localhost:8800" : "https://socialtnt.leevyy.click";
 
